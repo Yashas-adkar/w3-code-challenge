fb31384b346056d62f2f43f8186e478e227e1c0b
YASHAS ADKAR <yashasadkar@gmail.com>
2026-05-24 17:03:07 +0530

    Added for loop with continue statement

---
+++ forloopcontinue.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x) 
+fruits=["apple","banana","cherry"]
+for i in fruits:
+  if i=="cherry":
+     continue
+  print(i)
